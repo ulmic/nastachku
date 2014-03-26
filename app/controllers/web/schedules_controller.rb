087f07d0a6292159335852cfa117fbdf14d2b6b7
Dmitry Davydov <haudvd@gmail.com>
2014-03-26 21:15:41 +0400

    temporaly back

--- app/controllers/web/schedules_controller.rb
+++ app/controllers/web/schedules_controller.rb
@@ -2,8 +2,6 @@ class Web::SchedulesController < Web::ApplicationController
   def show
     # @halls = HallDecorator.decorate_collection Hall.includes(slots: {lecture: :workshop}).activated.by_position
     @halls = HallDecorator.decorate_collection Hall.joins(:slots).joins("LEFT OUTER JOIN lectures ON slots.event_id = lectures.id").where('lectures.id = slots.event_id').activated.by_position.uniq
-    raise @halls.inspect
-    @halls.slots = SlotDecorator.decorate_collection @halls.slots
     @slots = Slot.all
     @workshops = Workshop.all
   end
